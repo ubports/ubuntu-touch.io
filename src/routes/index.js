16d20287b767b3aa51eec3f54c11306f2c40a5cd
NeoTheThird <neo@neothethird.de>
2017-10-27 22:26:09 +0200

    Added apps section (WIP)

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/apps', function(req, res, next) {
+  res.render('apps');
+});
+
 router.get('/telegram', function(req, res, next) {
   res.redirect("https://telegram.me/ubports");
 });
