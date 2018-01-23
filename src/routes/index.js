0773352c20bd895a0478cfd1f427b37c352c1058
Marius Gripsgard <mariogrip@ubuntu.com>
2018-01-23 22:57:05 +0100

    Device page inital

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,85 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const time = () => Math.floor(new Date() / 1000)
+
+var cache = {
+  devices: {expire:0}
+}
+
+// Internal
+const getDevices = () => {
+  return new Promise(function(resolve, reject) {
+    console.log("devices request");
+    var now=time();
+
+    // Cache baby cache!!! :D :D
+    if (cache.devices.expire > now) {
+      console.log("use cache");
+      resolve(cache.devices.data);
+      return;
+    }
+
+    console.log("get new");
+    request({
+      method: "get",
+      url: "https://devices.ubports.com/api/devices",
+      json: true,
+      headers: {
+        'User-Agent': 'client request: server ubuntu-touch.io'
+      }
+    }, (err, res, body) => {
+      // If we hit an error, try using cache!
+      if (err)
+        resolve(cache.devices.data)
+      resolve(body);
+      // 3 munutes cache!
+      cache.devices.expire = time()+180;
+      cache.devices.data = body;
+    });
+  });
+}
+
+const getDevice = (device) => {
+  return new Promise(function(resolve, reject) {
+    console.log("devices request");
+    var now=time();
+
+    if (!cache[device])
+      cache[device] = {expire:0};
+
+    // Cache baby cache!!! :D :D
+    if (cache[device].expire > now) {
+      console.log("use cache")
+      resolve(cache[device].data);
+      return;
+    }
+
+    console.log("get new");
+    request({
+      method: "get",
+      url: "https://devices.ubports.com/api/device/"+device,
+      json: true,
+      headers: {
+        'User-Agent': 'client request: server ubuntu-touch.io'
+      }
+    }, (err, res, body) => {
+      // If we hit an error, try using cache!
+      if (err) {
+        if(res.statusCode === 404)
+          reject();
+        else
+          resolve(cache[device].data)
+        return;
+      }
+      resolve(body);
+      // 3 munutes cache!
+      cache[device].expire = time()+180;
+      cache[device].data = body;
+    });
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -32,6 +112,18 @@ router.get('/privacy', function(req, res, next) {
 
 router.get('/devices', function(req, res, next) {
   res.render('devices')
+})
+
+router.get('/api/devices', function(req, res, next) {
+  getDevices().then(r => res.send(r));
+})
+
+router.get('/api/device/:device', function(req, res, next) {
+  getDevice(req.params.device).then(r => res.send(r)).catch(() => res.send(404));
+})
+
+router.get('/device/:device', function(req, res, next) {
+  getDevice(req.params.device).then(r => res.render('device', { data: r })).catch(() => res.send(404));
 })
 
 router.get('/telegram', function(req, res, next) {
